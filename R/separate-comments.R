e31f51dcd63ac4c0d4dafb5f02bb77b4f2ca7321
Joe Thorley <joe@poissonconsulting.ca>
2016-09-15 15:29:43 -0700

    Messing with separate comments

--- R/separate-comments.R
+++ R/separate-comments.R
@@ -1,3 +1,5 @@
 separate_comments <- function(model_code) {
-  model_code
+  comments <- str_replace_all(model_code, "\n|(^|\n)[^#]*", "\n")
+  model_code %<>% str_replace_all("#[^\n]*", "") %>% str_replace_all("[ ]{2,}", "")
+  list(model_code = model_code, comments = comments)
 }
--- tests/testthat/test-separate-comments.R
+++ tests/testthat/test-separate-comments.R
@@ -2,4 +2,23 @@ context("separate-comments")
 
 test_that("separate_comments", {
 
+#   model_code <- "model {
+# bIntercept ~ dnorm(0, 5^-2) # M
+# sY ~ dunif(0, 5)            # M
+# for(i in 1:length(Y)) {     # MP
+# mu[i] <- bIntercept + bX * X[i]
+# Y2[i] ~ dnorm (mu[i], sY^-2) # a
+# }
+# }"
+#   separate_comments(model_code)$comments
+#   expect_identical(separate_comments(model_code)$model_code,
+#                  "model {
+# bIntercept ~ dnorm(0, 5^-2)
+# sY ~ dunif(0, 5)
+# for(i in 1:length(Y)) {
+# mu[i] <- bIntercept + bX * X[i]
+# Y2[i] ~ dnorm (mu[i], sY^-2)
+# }
+# }")
+
 })

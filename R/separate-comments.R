8764e18c3002124922ef25107185c8e139d00803
Thormidable <will.thorley@gmail.com>
2016-09-24 10:59:33 -0700

    UPDATE - removed uneccessary files

--- R/separate-comments.R
+++
@@ -1,5 +0,0 @@
-separate_comments <- function(model_code) {
-  comments <- str_replace_all(model_code, "\n|(^|\n)[^#]*", "\n")
-  model_code %<>% str_replace_all("#[^\n]*", "") %>% str_replace_all("[ ]{2,}", "")
-  list(model_code = model_code, comments = comments)
-}
